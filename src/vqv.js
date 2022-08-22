554adc4cdf9db9ef9812b5cc36f5e7476d0680d5
Felipe Gouveia <felipegouveia.bfr@hotmail.com>
2022-08-22 15:42:22 -0300

    tentando resolver a 3

--- src/vqv.js
+++ src/vqv.js
@@ -22,7 +22,8 @@ const vqv = (nome, idade) => {
   if (!nome || !idade) {
     return undefined;
   }
-  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\n
+  return `Oi, meu nome é ${nome}!\n
+  Tenho ${idade} anos,\n
   trabalho na Trybe e mando muito em programação!\n'#VQV!`;
 };
 
